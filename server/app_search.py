2078ff1050e110c201d47c29294acd59519a35ca
naim-prog <naimrodrey@protonmail.com>
2022-11-23 21:16:08 +0100

    Search working without reload page

--- server/app_search.py
+++ server/app_search.py
@@ -1,5 +1,5 @@
 # For the app
-from flask import Blueprint, render_template, request, send_file
+from flask import Blueprint, render_template, request, send_file, render_template_string
 
 # Blueprint for home
 app_search = Blueprint(__name__, "app_search")
@@ -8,12 +8,11 @@ app_search = Blueprint(__name__, "app_search")
 # User
 @app_search.get("/search/")
 def get_search():
-    
-    print(request.headers)
+    print(request.args.get('search'))
     # Render only body container
     if request.headers.get('Ajax-Render'):
-        return send_file(
-            "templates/search",
+        return render_template_string(
+            open("templates/search").read(),
             user_search = request.args.get('search')
         )
 
--- server/static/js/functions.js
+++ server/static/js/functions.js
@@ -2,6 +2,10 @@
 // from another endpoint of the web and we can still have
 // the header and the music player on the bottom of the page
 
+window.onload = function() {
+}
+
+
 function hrefClickHome() {
     $.ajax({
         url: '/',
@@ -9,10 +13,8 @@ function hrefClickHome() {
         type: 'GET',
         headers: {'Ajax-Render': true},
         success: function(msg, status) {
-            console.log(status);
-            if(status == 'success') {
+            if(status === 'success') {
                 history.replaceState('','','/');
-                console.log("url: /");
                 $('#body-container').html(msg);
             }
             document.title = 'WYG - Home';
@@ -27,10 +29,8 @@ function hrefClickUser() {
         type: 'GET',
         headers: {'Ajax-Render': true},
         success: function(msg, status) {
-            console.log(status);
-            if(status == 'success') {
+            if(status === 'success') {
                 history.replaceState('','','/user/');
-                console.log("url: /user/");
                 $('#body-container').html(msg);
             }
             document.title = 'WYG - User profile';
@@ -38,24 +38,20 @@ function hrefClickUser() {
     })
 }
 
-function formClickSearch() {
-    var search_text = document.getElementById('search-form-input-search').value;
-    console.log("/search/?search=" + search_text);
+
+function hrefSearch() {
+    var query = document.getElementById("search-form-input-search").value;
     $.ajax({
-        url: "/search/",
+        url: '/search/?search='+query,
         timeout: 5000,
         type: 'GET',
-        arguments: {"search": search_text},
         headers: {'Ajax-Render': true},
         success: function(msg, status) {
-            console.log(msg);
-            console.log(status);
-            if(status == 'success') {
-                history.replaceState('','','/search/');
-                console.log("url: /search/" + search_text);
+            if(status === 'success') {
+                history.replaceState('','','/search/?search='+query);
                 $('#body-container').html(msg);
             }
-            document.title = 'WYG - Search ' + search_text;
+            document.title = 'WYG - Search '+query;
         }
     })
 }
