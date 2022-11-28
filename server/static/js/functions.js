2974ef709a92f094534f7f9da82f17ffb667feee
naim-prog <naimrodrey@protonmail.com>
2022-11-28 09:19:31 +0100

    Search bar not reload when keypress 'Enter'

--- server/static/js/functions.js
+++ server/static/js/functions.js
@@ -39,19 +39,24 @@ function hrefClickUser() {
 }
 
 
-function hrefSearch() {
-    var query = document.getElementById("search-form-input-search").value;
-    $.ajax({
-        url: '/search/?search='+query,
-        timeout: 5000,
-        type: 'GET',
-        headers: {'Ajax-Render': true},
-        success: function(msg, status) {
-            if(status === 'success') {
-                history.replaceState('','','/search/?search='+query);
-                $('#body-container').html(msg);
+function hrefSearch(event) {
+    if(event.keyCode == 13) {
+        var query = document.getElementById("search-form-input-search").value;
+        $.ajax({
+            url: '/search/?search='+query,
+            timeout: 5000,
+            type: 'GET',
+            headers: {'Ajax-Render': true},
+            success: function(msg, status) {
+                if(status === 'success') {
+                    history.replaceState('','','/search/?search='+query);
+                    $('#body-container').html(msg);
+                }
+                document.title = 'WYG - Search '+query;
             }
-            document.title = 'WYG - Search '+query;
-        }
-    })
+        })
+        return true;
+    } else {
+        return false;
+    }
 }
