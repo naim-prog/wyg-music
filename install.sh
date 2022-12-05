520925311abde75f8d13f3e409c0f31fc586ff59
naim-prog <naimrodrey@protonmail.com>
2022-12-05 10:15:09 +0100

    Install script for python packages and tools #1

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
-# sudo apt install redis
+#!/bin/bash
+
+if [ $(id -u) != '0' ]
+then
+	echo "Run this script with sudo so you don't have any problems"
+	exit 1
+fi
+
+
+sudo apt install redis
 
 # pip install -r requirements.txt
