90cd49b1f0eb064b4502f8e638d7b48a96eceb0f
naim-prog <naimrodrey@protonmail.com>
2022-12-08 17:33:20 +0100

    Upload song files box center (not working)

--- server/app_songs.py
+++ server/app_songs.py
@@ -1,5 +1,5 @@
 # For the app
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, request, render_template, flash
 # Global variables
 from global_vars import *
 
@@ -12,3 +12,39 @@ app_songs = Blueprint(__name__, "app_songs")
 def get_song(song_name):
     return send_file(f"{BASE_SERVER_DIRECTORY}/songs/{song_name}", mimetype="audio/mp3")
 
+
+# Upload a song (GET PAGE)
+@app_songs.get("/upload/")
+def get_upload():
+
+    # Request from HTMX
+    if request.headers.get('Hx-Request'):
+        return render_template(
+            "upload.html",
+            render_music_player=False
+        )
+
+    # Render hole page (and MusicPlayer)
+    return render_template(
+        "upload.html",
+        render_music_player=True
+    )
+    
+
+# Upload a song (POST PAGE)
+@app_songs.post("/upload/")
+def post_upload():
+
+    # List of files uploaded
+    file_list = request.files.get('song-files')
+
+    # There is no song dropped but he push "upload" button
+    if file_list == None:
+        flash("You need to upload at least 1 song")
+        return render_template(
+            "upload.html",
+            render_music_player=False
+        )
+
+    # If there are songs
+    print(request.files.get('song-files'))
